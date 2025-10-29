7b6ef492d39b9013935d591bfd80b9ea926125f4
George Constantin <26378695+cnst-george@users.noreply.github.com>
2025-10-29 07:49:18 +0200

    changes in Readme

---
+++ list-resources/list-resources.sh
@@ -0,0 +1,19 @@
+﻿# !/bin/bash
+
+page=$(oci \
+    search \
+    resource \
+    structured-search \
+    --limit 1000 \
+    --query-text \
+    "QUERY all resources where (lifeCycleState != 'TERMINATED' || lifeCycleState != 'FAILED')" | \
+	   jq -r '."opc-next-page"')
+while [ -n $page ]; do
+    oci search resource structured-search --page $page --limit 1000 --query-text \
+	"QUERY all resources where (lifeCycleState != 'TERMINATED' || lifeCycleState != 'FAILED')" | \
+	jq -r '[.data.items[] | {compartmentId: ."compartment-id", displayName:."display-name" , id: .identifier, definedTags: ."defined-tags", freeformTags: ."freeform-tags", lifecycleState: ."lifecycle-state"}]'
+    old_page=$page
+    page=$(oci search resource structured-search --page $old_page --limit 1000 --query-text \
+	       "QUERY all resources where (lifeCycleState != 'TERMINATED' || lifeCycleState != 'FAILED')" | \
+	       jq -r '."opc-next-page"')
+done
